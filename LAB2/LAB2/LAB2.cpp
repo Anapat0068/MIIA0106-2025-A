a71c5a369a4145c5677ecde2765a93cc8cebf5e3
Anapat0068 <6711130068@mut.ac.th>
2025-11-25 16:39:44 +0700

    การทดลองครั้งที่ 2

---
+++ LAB2/LAB2/LAB2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Name: Anapat0068\n";
+    std::cout << "Course: MIIA0106-A\n";
+    std::cout << "Email: 6711130068@mut.ac.th\n";
+    return 0;
+}
